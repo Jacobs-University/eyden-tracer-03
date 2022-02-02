bea1c1877d6507138477212ec999fd3bca0b99d7
Mahmoud El Bergui <46870458+MaHmOuDeB@users.noreply.github.com>
2022-02-02 17:50:41 +0100

    Update BSPNode.h

--- src/BSPNode.h
+++ src/BSPNode.h
@@ -1,5 +1,3 @@
-// BSP node class for BSP trees
-// Written by Dr. Sergey G. Kosov in 2019 for Jacobs University
 #pragma once
 
 #include "types.h"
@@ -14,90 +12,112 @@ using ptr_bspnode_t = std::shared_ptr<CBSPNode>;
 class CBSPNode
 {
 public:
-	/**
-	 * @brief Leaf node constructor
-	 * @param vpPrims The vector of pointers to the primitives included in the leaf node
-	 */
-	CBSPNode(const std::vector<ptr_prim_t>& vpPrims)
-		: CBSPNode(vpPrims, 0, 0, nullptr, nullptr)
-	{}
-	/**
-	 * @brief Branch node constructor
-	 * @param splitDim The splitting dimension
-	 * @param splitVal The splitting value
-	 * @param left Pointer to the left sub-tree
-	 * @param right Pointer to the right sub-tree
-	 */
-	CBSPNode(int splitDim, float splitVal, ptr_bspnode_t left, ptr_bspnode_t right)
-		: CBSPNode(std::nullopt, splitDim, splitVal, left, right)
-	{}
-	CBSPNode(const CBSPNode&) = delete;
-	virtual ~CBSPNode(void) = default;
-	const CBSPNode& operator=(const CBSPNode&) = delete;
+    /**
+     * @brief Leaf node constructor
+     * @param vpPrims The vector of pointers to the primitives included in the leaf node
+     */
+    CBSPNode(const std::vector<ptr_prim_t>& vpPrims)
+        : CBSPNode(vpPrims, 0, 0, nullptr, nullptr)
+    {}
+    /**
+     * @brief Branch node constructor
+     * @param splitDim The splitting dimension
+     * @param splitVal The splitting value
+     * @param left Pointer to the left sub-tree
+     * @param right Pointer to the right sub-tree
+     */
+    CBSPNode(int splitDim, float splitVal, ptr_bspnode_t left, ptr_bspnode_t right)
+        : CBSPNode(std::nullopt, splitDim, splitVal, left, right)
+    {}
+    CBSPNode(const CBSPNode&) = delete;
+    virtual ~CBSPNode(void) = default;
+    const CBSPNode& operator=(const CBSPNode&) = delete;
 
-	/**
-	 * @brief Traverses the ray \b ray and checks for intersection with a primitive
-	 * @details If the intersection is found, \b ray.t is updated
-	 * @param[in,out] ray The ray
-	 * @param[in] t0 The distance from ray origin at which the ray enters the scene
-	 * @param[in] t1 The distance from ray origin at which the ray leaves the scene
-	 * @retval true If ray \b ray intersects any object
-	 * @retval false otherwise
-	 */
-	bool intersect(Ray& ray, double t0, double t1) const
-	{
-		if (isLeaf()) {
-			// --- PUT YOUR CODE HERE ---
-			return false;
-		} else {
-			// --- PUT YOUR CODE HERE ---
-			return false;
-		}
-	}
+    /**
+     * @brief Traverses the ray \b ray and checks for intersection with a primitive
+     * @details If the intersection is found, \b ray.t is updated
+     * @param[in,out] ray The ray
+     * @param[in] t0 The distance from ray origin at which the ray enters the scene
+     * @param[in] t1 The distance from ray origin at which the ray leaves the scene
+     * @retval true If ray \b ray intersects any object
+     * @retval false otherwise
+     */
+    bool intersect(Ray& ray, double t0, double t1) const
+    {
+        if (isLeaf()) {
+            // --- PUT YOUR CODE HERE ---
+            for (auto it = m_vpPrims.begin(); it != m_vpPrims.end(); ++it) {
+                auto& pPrim = *it;
+                pPrim->intersect(ray);
+            }
+            return (ray.hit && ray.t < t1 + Epsilon);
+        }
 
-	/**
-	 * @brief Returns the pointer to the \a left child
-	 * @returns The pointer to the root-node of the \a left sub-tree
-	 */
-	ptr_bspnode_t Left(void) const { return m_pLeft; }
-	/**
-	 * @brief Returns the pointer to the \a right child
-	 * @returns The pointer to the root-node of the \a right sub-tree
-	 */
-	ptr_bspnode_t Right(void) const { return m_pRight; }
+        else if(!isLeaf()) {
+            double d = (m_splitVal - ray.org[m_splitDim]) / ray.dir[m_splitDim];
 
-	
+            auto frontNode = (ray.dir[m_splitDim] < 0) ? Right() : Left();
+            auto backNode = (ray.dir[m_splitDim] < 0) ? Left() : Right();
+
+            if (d <= t0) {//intersect with left child only
+
+                return backNode->intersect(ray, t0, t1);
+            }
+            else if (d >= t1) {//intersect with right child only
+                // t0..t1 is totally in front of d, only go to front side
+                return frontNode->intersect(ray, t0, t1);
+            }
+            else {
+                // traveses both children. front one first, back one last
+                if (frontNode->intersect(ray, t0, d))
+                    return true;
+                return backNode->intersect(ray, d, t1);
+            }
+        }
+    }
+
+    /**
+     * @brief Returns the pointer to the \a left child
+     * @returns The pointer to the root-node of the \a left sub-tree
+     */
+    ptr_bspnode_t Left(void) const { return m_pLeft; }
+    /**
+     * @brief Returns the pointer to the \a right child
+     * @returns The pointer to the root-node of the \a right sub-tree
+     */
+    ptr_bspnode_t Right(void) const { return m_pRight; }
+
+    
 private:
-	/**
-	 * @brief Genereal private constructor
-	 * @details This constructor may contruct both types of the nodes (leaf and branch) and is to be called from public leaf- and branch-node contructors
-	 * @param vpPrims The vector of pointers to the primitives included in the leaf node
-	 * @param splitDim The splitting dimension
-	 * @param splitVal The splitting value
-	 * @param left Pointer to the left sub-tree
-	 * @param right Pointer to the right sub-tree
-	 */
-	CBSPNode(std::optional<std::vector<ptr_prim_t>> vpPrims, int splitDim, float splitVal, ptr_bspnode_t left, ptr_bspnode_t right)
-		: m_vpPrims(vpPrims.value_or(std::vector<ptr_prim_t>()))
-		, m_splitDim(splitDim)
-		, m_splitVal(splitVal)
-		, m_pLeft(left)
-		, m_pRight(right)
-	{}
+    /**
+     * @brief Genereal private constructor
+     * @details This constructor may contruct both types of the nodes (leaf and branch) and is to be called from public leaf- and branch-node contructors
+     * @param vpPrims The vector of pointers to the primitives included in the leaf node
+     * @param splitDim The splitting dimension
+     * @param splitVal The splitting value
+     * @param left Pointer to the left sub-tree
+     * @param right Pointer to the right sub-tree
+     */
+    CBSPNode(std::optional<std::vector<ptr_prim_t>> vpPrims, int splitDim, float splitVal, ptr_bspnode_t left, ptr_bspnode_t right)
+        : m_vpPrims(vpPrims.value_or(std::vector<ptr_prim_t>()))
+        , m_splitDim(splitDim)
+        , m_splitVal(splitVal)
+        , m_pLeft(left)
+        , m_pRight(right)
+    {}
 
-	/**
-	 * @brief Checks whether the node is either leaf or branch node
-	 * @retval true if the node is the leaf-node
-	 * @retval false if the node is a branch-node
-	 */
-	bool isLeaf(void) const { return (!m_pLeft && !m_pRight); }
-	
-	
+    /**
+     * @brief Checks whether the node is either leaf or branch node
+     * @retval true if the node is the leaf-node
+     * @retval false if the node is a branch-node
+     */
+    bool isLeaf(void) const { return (!m_pLeft && !m_pRight); }
+    
+    
 private:
-	std::vector<ptr_prim_t>	m_vpPrims;		///< The vector of pointers to the primitives included in the leaf node
-	int 					m_splitDim;		///< The splitting dimension
-	float 					m_splitVal;		///< The splitting value
-	ptr_bspnode_t 	        m_pLeft;		///< Pointer to the left sub-tree
-	ptr_bspnode_t 	        m_pRight;		///< Pointer to the right sub-tree
+    std::vector<ptr_prim_t>    m_vpPrims;        ///< The vector of pointers to the primitives included in the leaf node
+    int                     m_splitDim;        ///< The splitting dimension
+    float                     m_splitVal;        ///< The splitting value
+    ptr_bspnode_t             m_pLeft;        ///< Pointer to the left sub-tree
+    ptr_bspnode_t             m_pRight;        ///< Pointer to the right sub-tree
 };
-
