b161f3cb8edbc7845d96d8ec859c5ed856dcfe77
Mahmoud El Bergui <46870458+MaHmOuDeB@users.noreply.github.com>
2022-02-02 17:55:10 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -32,7 +32,7 @@ Mat RenderFrame(void)
 #ifdef WIN32
 	const std::string dataPath = "../data/";
 #else
-	const std::string dataPath = "../../data/";
+	const std::string dataPath = "/Users/mahmoudelbergui/Downloads/eyden-tracer-03-master/data/";
 #endif
 	CSolid solid(pShader, dataPath + "Torus Knot.obj");
 	scene.add(solid);
@@ -50,12 +50,21 @@ Mat RenderFrame(void)
 	Mat img(resolution, CV_32FC3);							// image array
 	Ray ray;                                          		// primary ray
 
-	for (int y = 0; y < img.rows; y++)
-		for (int x = 0; x < img.cols; x++) {
-			scene.getActiveCamera()->InitRay(ray, x, y);	// initialize ray
-			img.at<Vec3f>(y, x) = scene.RayTrace(ray);
-		}
+//    for (int y = 0; y < img.rows; y++)
+//         for (int x = 0; x < img.cols; x++) {
+//             scene.getActiveCamera()->InitRay(ray, x, y);    // initialize ray
+//             img.at<Vec3f>(y, x) = scene.RayTrace(ray);
+//     }
 	
+    parallel_for_(Range(0, img.rows) , [&](const Range& range){
+        for(int y= range.start ; y < range.end ; y++)
+            for (int x = 0; x < img.cols; x++) {
+                Ray ray;
+                scene.getActiveCamera()->InitRay(ray, x, y);    // initialize ray
+                img.at<Vec3f>(y, x) = scene.RayTrace(ray);
+        }
+    });
+    
 	img.convertTo(img, CV_8UC3, 255);
 	return img;
 }
@@ -67,6 +76,6 @@ int main(int argc, char* argv[])
 	DirectGraphicalModels::Timer::stop();
 	imshow("Image", img);
 	waitKey();
-	imwrite("D:/renders/torus knot.jpg", img);
+	imwrite("/Users/mahmoudelbergui/Downloads/eyden-tracer-03-master/renders/ torus knot.jpg", img);
 	return 0;
 }
